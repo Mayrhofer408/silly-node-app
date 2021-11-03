d700be41c1efd8020bf892816877a14fee8b3c55
Mayrhofer408 <--global>
2021-11-03 22:31:06 +0100

    Prompt the user for breed to display
    Sadly the dog.ceo API doesn't contain images of my favourite breed,
    Lagotto Romagnolo, so i couldn't hard code ONE breed.
    
    This commit let's the user choose from any breed available in the api.
    For user interface I use prompts, which has performed great in previous
    projects.
    
    Since the format of the breeds list comming from the dog.ceo API isn't
    formatted in a way that's practical for our case, I flatten it. I hope
    the flattening function makes sense to you, otherwise I could expand it
    with some input-output examples or perhapts a unit test.

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
 const agent = require('superagent');
 const asciify = require('asciify-image');
+const prompts = require('prompts');
 
 const DOG_API_URL = 'https://dog.ceo/api';
 
+const getBreedList = async () => {
+  const dogApiBreedsEndpointUrl = `${DOG_API_URL}/breeds/list/all`;
+  const {
+    body: {message: breedDict},
+  } = await agent.get(dogApiBreedsEndpointUrl);
+  return breedDict;
+};
+
 const getDogByBreedImageUrl = async (breed) => {
-  const dogApiEndpointUrl = `${DOG_API_URL}/breed/${breed}/images/random`;
-  const { body: { message: dogUrl }} = await agent.get(dogApiEndpointUrl);
+  const dogApiImageEndpointUrl = `${DOG_API_URL}/breed/${breed}/images/random`;
+  const {
+    body: {message: dogUrl},
+  } = await agent.get(dogApiImageEndpointUrl);
   return dogUrl;
 };
 
+function flattenBreedDict(breedDict) {
+  const flatBreedList = [];
+  Object.entries(breedDict).forEach(([breed, breedPrefixes]) => {
+    if (!breedPrefixes || breedPrefixes.length < 1) {
+      flatBreedList.push(breed);
+    } else {
+      breedPrefixes.forEach((prefix) => {
+        flatBreedList.push(`${prefix} ${breed}`);
+      });
+    }
+  });
+
+  return flatBreedList;
+}
+
+const promptForBreed = async (availableBreeds) => {
+  const {value} = await prompts({
+    type: 'autocomplete',
+    name: 'value',
+    message: 'Pick a breed, any breed:',
+    choices: availableBreeds.map((breed) => ({title: breed})),
+  });
+  if (!value) {
+    throw new Error('User did not pick a breed!');
+  }
+  return value;
+};
+
 (async () => {
-  // Get a URL for a random image of a dog
-  const dogUrl = await getDogByBreedImageUrl('bulldog/boston');
+  // Get hierarchical dict of all breeds from API
+  const breedListAsDict = await getBreedList();
+
+  // Flatten breed dict into array of strings
+  const breedList = flattenBreedDict(breedListAsDict);
+
+  // Get a single breed string by prompting the user
+  const pickedBreed = await promptForBreed(breedList);
+
+  // Get a URL for a random image of a dog with picked breed from API
+  const dogUrl = await getDogByBreedImageUrl(pickedBreed);
 
   // Download the image
-  const { body: data } = await agent.get(dogUrl);
+  const {body: data} = await agent.get(dogUrl);
 
   // Convert the image to ascii
   const ascii = await asciify(data, {
